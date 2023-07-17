8c0dd66d5a840bfffe17868fd9c14b919afb967d
Nikolay Osetrov <osetrov.n@gmail.com>
2023-07-18 00:42:08 +0200

    17.07.2023 JavaBasic

---
+++ 20230717Basic/src/main/java/com/nosetr/main/App.java
@@ -0,0 +1,57 @@
+package com.nosetr.main;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author nosetr
+ */
+public class App {
+    
+    private String firstName;
+    
+    private String lastName;
+
+    public String getFullName() {
+        return String.format("%s %s", firstName, this.lastName);
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+
+    public static void main(String[] args) {
+//        int value;
+//        value = 12345;
+//        
+//        while (value != 0) {
+//            System.out.println(value % 10);
+//            value = value / 10;
+//        }
+
+//        Scanner sc = new Scanner(System.in);
+//        int value = 123456;
+//        
+//        System.out.println("value: ");
+        
+//        По двум заданным числам проверять является ли одно квадратом другого
+        double a, b;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a: ");
+        a = sc.nextDouble();
+        System.out.println("Enter b: ");
+        b = sc.nextDouble();
+        
+        double result1 = a * a, result2 = b * b;
+        String result;
+        
+        result = (result1 == b || result2 == a) ? "YES" : "NO";
+        
+        System.out.println(result);
+    }
+}
